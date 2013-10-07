c33ec0499da5dcf2125ba696b920a0ce999cad69
Leloiandudu <leloiandudu@gmail.com>
2013-10-07 23:49:23 +0400

    + MediaWiki

---
+++ Browser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+class Browser
+{
+    private readonly CookieContainer _cookies = new CookieContainer();
+
+    public string UserAgent { get; set; }
+
+    public string Get(string url, Dictionary<string, string> args)
+    {
+        return GetStringResponse(GetRequest(string.Format("{0}?{1}", url, ArgsToString(args))));
+    }
+
+    public string Post(string url, Dictionary<string, string> args)
+    {
+        var request = GetRequest(url);
+        request.Method = "POST";
+        request.ContentType = "application/x-www-form-urlencoded";
+
+        var data = ArgsToString(args);
+        using (var stream = request.GetRequestStream())
+        {
+            var buf = Encoding.ASCII.GetBytes(data);
+            stream.Write(buf, 0, buf.Length);
+        }
+
+        return GetStringResponse(request);
+    }
+
+    private HttpWebRequest GetRequest(string url)
+    {
+        var request = (HttpWebRequest)WebRequest.Create(url);
+        request.CookieContainer = _cookies;
+        request.UserAgent = UserAgent;
+        request.ServicePoint.Expect100Continue = false;
+        return request;
+    }
+
+    private string ArgsToString(Dictionary<string, string> args)
+    {
+        return string.Join("&", (
+            from p in args
+            where p.Value != null
+            select string.Format("{0}={1}", Uri.EscapeDataString(p.Key), Uri.EscapeDataString(p.Value))
+        ).ToArray());
+    }
+
+    private string GetStringResponse(HttpWebRequest request)
+    {
+        using (var response = (HttpWebResponse)request.GetResponse())
+        using (var sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            return sr.ReadToEnd();
+    }
+}
---
+++ MediaWiki.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+class MediaWiki
+{
+    private readonly Browser _browser;
+    private readonly Uri _apiUri;
+
+    public MediaWiki(Uri apiUri, string userAgent)
+    {
+        _browser = new Browser { UserAgent = userAgent };
+        _apiUri = apiUri;
+    }
+
+    private static string JoinList(IEnumerable<string> tokens)
+    {
+        return string.Join("|", tokens.ToArray());
+    }
+
+    public void Login(string login, string password)
+    {
+        var res = DoLogin(login, password)["login"];
+        if (res.Value<string>("result") == "NeedToken")
+            res = DoLogin(login, password, res.Value<string>("token"))["login"];
+        if (res.Value<string>("result") != "Success")
+            throw new MediaWikiException(res.Value<string>("result"));
+    }
+
+    private JToken DoLogin(string login, string password, string token = null)
+    {
+        return Exec(
+            new Dictionary<string, string>
+            {
+                { "format", "json" },
+                { "action", "login" },
+                { "lgname", login },
+                { "lgpassword", password },
+                { "lgtoken", token },
+            }
+        );
+    }
+
+    private IDictionary<string, JToken> Query(IDictionary<string, string> queryArgs, params string[] titles)
+    {
+        var args = new Dictionary<string, string>
+        {
+            { "format", "json" },
+            { "action", "query" },
+            { "titles", JoinList(titles) },
+        };
+
+        foreach (var arg in queryArgs)
+            args.Add(arg.Key, arg.Value);
+
+        var res = Exec(args);
+
+        return res["query"]["pages"].Values().ToDictionary(x => x.Value<string>("title"));
+    }
+
+    private JToken GetTokens(params string[] types)
+    {
+        var args = new Dictionary<string, string>
+            {
+                { "format", "json" },
+                { "action", "tokens" },
+            };
+        if (types.Any())
+            args.Add("type", JoinList(types));
+
+        return JToken.Parse(_browser.Get(_apiUri.ToString(), args));
+    }
+
+    private string _editToken;
+    private string GetEditToken()
+    {
+        if (_editToken == null)
+        {
+            var res = GetTokens();
+            _editToken = (string)res.SelectToken("tokens.edittoken");
+            if (_editToken == null)
+                throw new Exception(res.ToString());
+        }
+
+        return _editToken;
+    }
+
+    public void Edit(string title, string contents, string summary)
+    {
+        var result = Exec(new Dictionary<string, string>
+        {
+            { "format", "json" },
+            { "action", "edit" },
+            { "title", title },
+            { "text", contents },
+            { "summary", summary },
+            { "token", GetEditToken() },
+        });
+
+        var code = result["edit"].Value<string>("result");
+        if (code == null)
+            throw new MediaWikiException("Invalid response: " + result);
+        if (code != "Success")
+            throw new MediaWikiException(code);
+    }
+
+    private JToken Exec(Dictionary<string, string> args)
+    {
+        var result = JToken.Parse(_browser.Post(_apiUri.ToString(), args));
+        if (result["error"] != null)
+            throw new MediaWikiException(result["error"].Value<string>("info"));
+        return result;
+    }
+}
+
+[Serializable]
+public class MediaWikiException : Exception
+{
+    public MediaWikiException() { }
+    public MediaWikiException(string message) : base(message) { }
+    public MediaWikiException(string message, Exception inner) : base(message, inner) { }
+    protected MediaWikiException(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+        : base(info, context) { }
+}
