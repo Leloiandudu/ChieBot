1d1f164eb20ba4c9cb07a0980e5c661e09b2bb89
Leloiandudu <leloiandudu@gmail.com>
2013-10-20 21:17:55 +0400

    + removing issue from preparation timetable

---
+++ DYK/NextIssuePreparationHeader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ChieBot.DYK
+{
+    class NextIssuePreparationHeader : PartiallyParsedWikiText<NextIssuePreparationHeader.Item>
+    {
+        private static readonly Regex TimetableItem = new Regex(@"^\s*\|\{\{злвч\|.*?\|(?<date>\d+ \w+)\|\+\}\}.*?\n\s*\|-\s*\n", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.ExplicitCapture);
+
+        public NextIssuePreparationHeader(string text)
+            : base(text, TimetableItem, x => new Item(x))
+        {
+        }
+
+        public class Item
+        {
+            public Item(Match match)
+            {
+                DateTime date;
+                if (!Utils.TryParseIssueDate(match.Groups["date"].Value, out date))
+                    throw new DidYouKnowException(string.Format("Не удалось распарсить дату выпуска `{0}`", match.Groups["date"].Value));
+                Date = date;
+            }
+
+            public DateTime Date { get; private set; }
+        }
+    }
+}
---
+++ PartiallyParsedWikiText.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace ChieBot
+{
+    class PartiallyParsedWikiText<T> : IEnumerable<T>
+        where T : class
+    {
+        private readonly List<Tuple<string, T>> _items = new List<Tuple<string, T>>();
+
+        public PartiallyParsedWikiText(string text, Regex regex, Func<Match, T> itemFactory)
+        {
+            var index = 0;
+            foreach (Match match in regex.Matches(text))
+            {
+                if (match.Index != index)
+                {
+                    _items.Add(Tuple.Create(text.Substring(index, match.Index - index), (T)null));
+                    index = match.Index;
+                }
+
+                _items.Add(Tuple.Create(match.Value, itemFactory(match)));
+                index += match.Length;
+            }
+
+            if (index != text.Length)
+                _items.Add(Tuple.Create(text.Substring(index), (T)null));
+
+            Debug.Assert(text == Text);
+        }
+
+        public bool Remove(T item)
+        {
+            return _items.RemoveAll(x => x.Item2 == item) > 0;
+        }
+
+        public string Text
+        {
+            get { return string.Join("", _items.Select(x => x.Item1)); }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.Select(x => x.Item2).Where(x => x != null).GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
