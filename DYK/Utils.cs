8ebc5b98fda254f5aefffc7f6dfa7e672a406116
Leloiandudu <leloiandudu@gmail.com>
2013-10-26 15:44:48 +0400

    * mono incorrect russian months names workaround

--- DYK/Utils.cs
+++ DYK/Utils.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace ChieBot.DYK
 {
     static class Utils
     {
+        // mono has incorrect russian months names :(
+        private static readonly string[] MonthNames = new[] { 
+            "января", "февраля", "марта", "апреля", "мая", "июня", 
+            "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+
+        private static readonly Regex DateRegex = new Regex(@"^(\d+) (\w+)$", RegexOptions.Compiled);
+
         public static bool TryParseIssueDate(string text, out DateTime date)
         {
-            if (!DateTime.TryParseExact(text, "d MMMM", CultureInfo.GetCultureInfo("ru-RU"), DateTimeStyles.None, out date))
+            date = DateTime.MinValue;
+
+            var match = DateRegex.Match(text);
+            if (!match.Success)
                 return false;
-            if ((DateTime.Now - date).TotalDays > 30) // на случай анонсов для следующего года
+
+            var month = Array.IndexOf(MonthNames, match.Groups[2].Value);
+            if (month == -1)
+                return false;
+
+            date = new DateTime(DateTime.UtcNow.Year, month + 1, int.Parse(match.Groups[1].Value));
+
+            if ((DateTime.Now - date).TotalDays > 30) // in case of announces for next year 
                 date = date.AddYears(1);
             return true;
         }
