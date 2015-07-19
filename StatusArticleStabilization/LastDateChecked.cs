a57545c7273fb4ae87a3ddad2234f25252bb39f7
Leloiandudu <leloiandudu@gmail.com>
2015-07-19 20:18:57 +1200

    + SAS

---
+++ StatusArticleStabilization/LastDateChecked.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace ChieBot.StatusArticleStabilization
+{
+    class LastDateChecked
+    {
+        private static readonly string Filename = Path.Combine(Utils.GetProgramDir(), "sas-lastrev");
+
+        public DateTimeOffset? Get()
+        {
+            try
+            {
+                var text = File.ReadAllText(Filename);
+                long ticks;
+                if (long.TryParse(text, out ticks))
+                    return new DateTimeOffset(ticks, TimeSpan.Zero);
+            }
+            catch(FileNotFoundException)
+            {
+            }
+
+            return null;
+        }
+
+        public void Set(DateTimeOffset value)
+        {
+            File.WriteAllText(Filename, value.ToUniversalTime().Ticks.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
