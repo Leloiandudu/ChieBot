fc3dedb006caedd83692860bd2f9ec844176a4ea
Leloiandudu <2293329+Leloiandudu@users.noreply.github.com>
2015-10-23 22:38:25 +1300

    + FLS

---
+++ FeaturedListStabilization/FLSModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using ChieBot.Modules;
+
+namespace ChieBot.FeaturedListStabilization
+{
+    class FLSModule : IModule
+    {
+        private static readonly Regex LinkRegex = new Regex(@"\[\[(?!(User|У|Участник|User talk|ОУ|Обсуждение участника|File|Image|Файл):)(?<link>[^|#\]]+)(#[^|\]]*)?(\|[^\]]*)?\]\]", RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture);
+
+        public void Execute(MediaWiki wiki, string[] commandLine, Credentials credentials)
+        {
+            wiki.Login(credentials.Login, credentials.Password);
+            Stabilize(wiki, "Шаблон:Текущий избранный список");
+        }
+
+        private void Stabilize(MediaWiki wiki, string currentTemplateTitle)
+        {
+            var text = wiki.GetPage(currentTemplateTitle);
+            var link = LinkRegex.Match(text);
+            if (!link.Success)
+                throw new Exception("Current featured list link not found.");
+
+            wiki.Stabilize(link.Groups["link"].Value, "Автоматическая стабилизация избранного списка.", null);
+        }
+    }
+}
