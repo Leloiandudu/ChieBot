952e56e9731e5c0423c46c22026749b8f5a14c35
Leloiandudu <leloiandudu@gmail.com>
2018-02-11 15:54:45 +1300

    + Meir? archiving

--- Archiving/ArchiveModule.cs
+++ Archiving/ArchiveModule.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace ChieBot.Archiving
 {
@@ -11,7 +10,7 @@ namespace ChieBot.Archiving
 
         private readonly IDictionary<string, IArchiveRules> _rules = new IArchiveRules[]
         {
-            new LeLoiArchiveRules(),
+            new DelegateArchiveRules("Ле Лой", d => GetLeLoyArchiveName(d).ToString()),
             new GeneralArchiveRules("AnimusVox") { AgeLimitInDays = 14 },
             new GeneralArchiveRules("Milez189"),
             new GeneralArchiveRules("Lazyhawk") { ArchiveStartYear = 2008 },
@@ -19,6 +18,7 @@ namespace ChieBot.Archiving
             new GeneralArchiveRules("The222anonim") { ArchiveStartYear = 2014 },
             new GeneralArchiveRules("Пппзз") { ArchiveStartYear = 2016 },
             new GeneralArchiveRules("НоуФрост"),
+            new DelegateArchiveRules("Meiræ", d => ""),
         }.ToDictionary(x => x.UserName);
 
         public void Execute(MediaWiki wiki, string[] commandLine, Credentials credentials)
@@ -54,6 +54,20 @@ namespace ChieBot.Archiving
             wiki.Edit(talkName, talks.FullText, EditSummary);
         }
 
+        private static int GetLeLoyArchiveName(DateTime date)
+        {
+            if (date.Year <= 2012)
+                throw new ArgumentOutOfRangeException();
+
+            if (date.Year == 2013)
+                return 2;
+
+            if (date.Year == 2014)
+                return date.Month <= 6 ? 3 : 4;
+
+            return 5 + (date.Year - 2015) * 4 + (date.Month - 1) / 3;
+        }
+
         interface IArchiveRules
         {
             string UserName { get; }
@@ -61,63 +75,55 @@ namespace ChieBot.Archiving
             int AgeLimitInDays { get; }
         }
 
-        class LeLoiArchiveRules : IArchiveRules
-        {
-            private const string ArchiveName = "Архив{0}";
-
-            public string UserName
-            {
-                get { return "Ле Лой"; }
-            }
-
-            public string GetArchiveName(DateTime date)
-            {
-                return string.Format(ArchiveName, GetArchiveNamePrefix(date));
-            }
-
-            private static int? GetArchiveNamePrefix(DateTime date)
-            {
-                if (date.Year <= 2012)
-                    throw new ArgumentOutOfRangeException();
-
-                if (date.Year == 2013)
-                    return 2;
-
-                if (date.Year == 2014)
-                    return date.Month <= 6 ? 3 : 4;
-
-                return 5 + (date.Year - 2015) * 4 + (date.Month - 1) / 3;
-            }
-
-            public int AgeLimitInDays
-            {
-                get { return 7; }
-            }
-        }
-
-        class GeneralArchiveRules : IArchiveRules
+        abstract class ArchiveRulesBase : IArchiveRules
         {
             private readonly string _userName;
 
-            public GeneralArchiveRules(string userName)
+            public ArchiveRulesBase(string userName)
             {
                 _userName = userName;
                 AgeLimitInDays = 7;
             }
 
-            private const string ArchiveName = "Архив/{0}";
-
-            public string UserName
-            {
-                get { return _userName; }
-            }
-
-            public string GetArchiveName(DateTime date)
-            {
-                return string.Format(ArchiveName, date.Year - ArchiveStartYear);
-            }
-
             public int AgeLimitInDays { get; set; }
+
+            public string UserName { get { return _userName; } }
+
+            protected abstract string GetArchiveName(DateTime date);
+
+            string IArchiveRules.GetArchiveName(DateTime date)
+            {
+                return "Архив" + GetArchiveName(date);
+            }
+        }
+
+        class DelegateArchiveRules : ArchiveRulesBase
+        {
+            private readonly Func<DateTime, string> _getArchiveName;
+
+            public DelegateArchiveRules(string userName, Func<DateTime, string> getArchiveName)
+                : base(userName)
+            {
+                _getArchiveName = getArchiveName;
+            }
+
+            protected override string GetArchiveName(DateTime date)
+            {
+                return _getArchiveName(date);
+            }
+        }
+
+        class GeneralArchiveRules : ArchiveRulesBase
+        {
+            public GeneralArchiveRules(string userName)
+                : base(userName)
+            {
+            }
+
+            protected override string GetArchiveName(DateTime date)
+            {
+                return "/" + (date.Year - ArchiveStartYear);
+            }
 
             public int ArchiveStartYear { get; set; }
         }
