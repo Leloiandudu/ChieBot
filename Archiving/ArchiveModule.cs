d57bd5dd61c71b249df0884cae3319433db4726c
Leloiandudu <2293329+Leloiandudu@users.noreply.github.com>
2023-12-29 18:38:24 +1300

    archiving revamp

--- Archiving/ArchiveModule.cs
+++ Archiving/ArchiveModule.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace ChieBot.Archiving
@@ -8,124 +7,98 @@ namespace ChieBot.Archiving
     {
         private const string EditSummary = "Автоматический аркайвинг";
 
-        private readonly IDictionary<string, IArchiveRules> _rules = new IArchiveRules[]
+        private readonly ArchiveRules[] _rules = new[]
         {
-            new DelegateArchiveRules("Ле Лой", d => $"Архив{GetLeLoyArchiveName(d)}"),
-            new GeneralArchiveRules("AnimusVox") { AgeLimitInDays = 14 },
-            new GeneralArchiveRules("Milez189"),
-            new GeneralArchiveRules("Lazyhawk") { ArchiveStartYear = 2008 },
-            new GeneralArchiveRules("Ping08"),
-            new GeneralArchiveRules("The222anonim") { ArchiveStartYear = 2014 },
-            new GeneralArchiveRules("Евгений Юрьев") { ArchiveStartYear = 2016 },
-            new GeneralArchiveRules("Люба КБ"),
-            new DelegateArchiveRules("Meiræ", d => "Архив"),
-            new GeneralArchiveRules("Stjn"),
-            new GeneralArchiveRules("Red Blooded Man") { ArchiveStartYear = 2019 },
-            new GeneralArchiveRules("Alex parker 1979") { ArchiveStartYear = 2019 },
-            new GeneralArchiveRules("Putnik") { AgeLimitInDays = 90 },
-            new GeneralArchiveRules("Iniquity") { ArchiveStartYear = 2014 },
-            new DelegateArchiveRules("Рейму Хакурей", d => d.Year.ToString()) { AgeLimitInDays = 30 },
-            new GeneralArchiveRules("Birulik") { ArchiveStartYear = 2017, AgeLimitInDays = 90 },
-        }.ToDictionary(x => x.UserName);
+            Rules("Ле Лой", Daily, GetLeLoyArchiveName),
+            Rules("AnimusVox", Weekly, ageLimitInDays: 14),
+            Rules("Lazyhawk", Daily, SimpleArchiveName(2008)),
+            Rules("Ping08", Weekly),
+            Rules("The222anonim", Weekly, SimpleArchiveName(2014)),
+            Rules("Meiræ", Weekly, d => "Архив"),
+            Rules("Stjn", TriMonthly),
+            Rules("Alex parker 1979", Weekly, SimpleArchiveName(2019)),
+            Rules("Putnik", Weekly, ageLimitInDays:90),
+            Rules("Birulik", TriMonthly, SimpleArchiveName(2017), ageLimitInDays: 90),
+        };
 
         public void Execute(MediaWiki wiki, string[] commandLine)
         {
-            if (commandLine.Length != 1)
-                throw new Exception("specify user name as a single parameter");
-
-            var rules = _rules[commandLine.Single()];
-            var talkName = string.Format("Обсуждение участника:{0}", rules.UserName);
-
             var now = DateTimeOffset.UtcNow;
-            var archiveName = string.Format("{0}/{1}", talkName, rules.GetArchiveName(now.Date));
 
-            var talks = new Talks(wiki.GetPage(talkName));
-            var dayX = now.AddDays(-rules.AgeLimitInDays);
-            var removed = new Talks();
-
-            foreach (var talk in talks.ToArray())
+            foreach (var rules in _rules)
             {
-                if (talk.LastActivity.HasValue && talk.LastActivity < dayX)
+                if (!rules.ShouldRun(now.Date))
+                    continue;
+
+                var talkName = string.Format("Обсуждение участника:{0}", rules.UserName);
+
+                var talks = new Talks(wiki.GetPage(talkName));
+                var dayX = now.AddDays(-rules.AgeLimitInDays);
+                var removed = new Talks();
+
+                foreach (var talk in talks.ToArray())
                 {
-                    talks.Remove(talk);
-                    removed.Add(talk);
+                    if (talk.LastActivity < dayX)
+                    {
+                        talks.Remove(talk);
+                        removed.Add(talk);
+                    }
                 }
-            }
 
-            if (!removed.Any())
-                return;
+                if (!removed.Any())
+                    return;
 
-            wiki.Edit(archiveName, "\n\n" + removed.FullText, EditSummary, true);
-            wiki.Edit(talkName, talks.FullText, $"[[{archiveName}|{EditSummary}]]");
-        }
-
-        private static int GetLeLoyArchiveName(DateTime date)
-        {
-            if (date.Year <= 2012)
-                throw new ArgumentOutOfRangeException();
-
-            if (date.Year == 2013)
-                return 2;
-
-            if (date.Year == 2014)
-                return date.Month <= 6 ? 3 : 4;
-
-            return 5 + (date.Year - 2015) * 4 + (date.Month - 1) / 3;
-        }
-
-        interface IArchiveRules
-        {
-            string UserName { get; }
-            string GetArchiveName(DateTime date);
-            int AgeLimitInDays { get; }
-        }
-
-        abstract class ArchiveRulesBase : IArchiveRules
-        {
-            private readonly string _userName;
-
-            public ArchiveRulesBase(string userName)
-            {
-                _userName = userName;
-                AgeLimitInDays = 7;
-            }
-
-            public int AgeLimitInDays { get; set; }
-
-            public string UserName { get { return _userName; } }
-
-            public abstract string GetArchiveName(DateTime date);
-        }
-
-        class DelegateArchiveRules : ArchiveRulesBase
-        {
-            private readonly Func<DateTime, string> _getArchiveName;
-
-            public DelegateArchiveRules(string userName, Func<DateTime, string> getArchiveName)
-                : base(userName)
-            {
-                _getArchiveName = getArchiveName;
-            }
-
-            public override string GetArchiveName(DateTime date)
-            {
-                return _getArchiveName(date);
+                var archiveName = string.Format("{0}/{1}", talkName, rules.GetArchiveName(now.Date));
+                wiki.Edit(archiveName, "\n\n" + removed.FullText, EditSummary, true);
+                wiki.Edit(talkName, talks.FullText, $"[[{archiveName}|{EditSummary}]]");
             }
         }
 
-        class GeneralArchiveRules : ArchiveRulesBase
+        class ArchiveRules
         {
-            public GeneralArchiveRules(string userName)
-                : base(userName)
+            public ArchiveRules(string userName, Predicate<DateTime> shouldRun, Func<DateTime, string> getArchiveName = null, int ageLimitInDays = 7)
             {
+                UserName = userName;
+                ShouldRun = shouldRun;
+                GetArchiveName = getArchiveName ?? SimpleArchiveName();
+                AgeLimitInDays = ageLimitInDays;
             }
 
-            public override string GetArchiveName(DateTime date)
-            {
-                return "Архив/" + (date.Year - ArchiveStartYear);
-            }
+            public string UserName { get; }
+            public Predicate<DateTime> ShouldRun { get; }
+            public int AgeLimitInDays { get; }
+            public Func<DateTime, string> GetArchiveName { get; }
+        }
 
-            public int ArchiveStartYear { get; set; }
+        private static ArchiveRules Rules(string userName, Predicate<DateTime> shouldRun, Func<DateTime, string> getArchiveName = null, int ageLimitInDays = 7)
+            => new ArchiveRules(userName, shouldRun, getArchiveName, ageLimitInDays);
+
+        private static bool Daily(DateTime _) => true;
+        private static bool Weekly(DateTime d) => d.DayOfWeek == DayOfWeek.Wednesday;
+        private static bool TriMonthly(DateTime d) => d.Day == 1 && d.Month % 3 == 1;
+
+        private static Func<DateTime, string> SimpleArchiveName(int archiveStartYear = 0)
+        {
+            return date => "Архив/" + (date.Year - archiveStartYear);
+        }
+
+        private static string GetLeLoyArchiveName(DateTime date)
+        {
+            Func<int> GetArchiveName = () =>
+            {
+                if (date.Year <= 2012)
+                    throw new ArgumentOutOfRangeException();
+
+                if (date.Year == 2013)
+                    return 2;
+
+                if (date.Year == 2014)
+                    return date.Month <= 6 ? 3 : 4;
+
+                return 5 + (date.Year - 2015) * 4 + (date.Month - 1) / 3;
+            };
+
+            return $"Архив{GetArchiveName()}";
         }
     }
 }
